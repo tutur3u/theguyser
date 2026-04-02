1c33c1877ce77a689cb8d6756c74c2cfdfb7edbf
Võ Hoàng Phúc <contact@vohoangphuc.com>
2026-04-02 16:31:42 +0700

    chore: update git lfs tracking

--- CLAUDE.md
+++ CLAUDE.md
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:336cc4fbf19beaada7ccf9986414fa91851a8d7a07dfb3ccbe800a69eed0ab49
-size 11
+@AGENTS.md
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,20 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:688ff71abfdf621f323d486dbd96301f28f74f82a37283c585ef1303b9aae7d9
-size 514
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    "public/games/**",
+    "wii-u-portfolio/**",
+  ]),
+]);
+
+export default eslintConfig;
